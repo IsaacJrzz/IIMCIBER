fc79c059be120a6d9dc501f77655185e19e6ef6a
Iker <ikerdc2005@gmail.com>
2026-01-13 10:44:40 +0100

    Update CyberSecurityMonitor.java

--- src/core/CyberSecurityMonitor.java
+++ src/core/CyberSecurityMonitor.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package core;
 
-/**
- *
- * @author Usuario
- */
+import java.nio.file.Path;
+
 public class CyberSecurityMonitor {
-    int a;
+    public static void main(String[] args) {
+        Path watchFolder  = Path.of("data/watch");
+        Path trafficLog   = Path.of("data/traffic.log");
+        Path processesLog = Path.of("data/processes.log");
+
+        SDASLogger logger = new SDASLogger(Path.of("log_sdas.txt"));
+        SDASService service = new SDASService(watchFolder, trafficLog, processesLog, logger);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(service::stop));
+        service.start();
+    }
 }
