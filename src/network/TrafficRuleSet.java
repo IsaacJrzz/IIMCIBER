e716f7d8fc5e194b4fbdcb11199dc04fb7533bfd
Iker <ikerdc2005@gmail.com>
2026-01-15 18:21:33 +0100

    Refactor TrafficRuleSet by cleaning comments and adding ports
    Removed unused comments and added a set of suspicious ports.

--- src/network/TrafficRuleSet.java
+++ src/network/TrafficRuleSet.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package network;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Set;
+
 public class TrafficRuleSet {
-    
+
+    private final Set<Integer> suspiciousPorts = Set.of(22, 23, 4444, 135, 3389);
+
+    public boolean isSuspiciousPort(int port) {
+        return suspiciousPorts.contains(port);
+    }
 }
